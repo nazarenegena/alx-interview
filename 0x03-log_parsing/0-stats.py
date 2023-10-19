02fb67df8e5baab3c8c28cdc813792e58821abc9
nazarenegena <genarene96@gmail.com>
2023-10-19 03:23:57 -0700

    refactored code

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -7,25 +7,25 @@ import re
 def extract_input(input_line):
     '''extracting sections of a line.
     '''
-    fp = (
+    pattern = (
         r'\s*(?P<ip>\S+)\s*',
         r'\s*\[(?P<date>\d+\-\d+\-\d+ \d+:\d+:\d+\.\d+)\]',
         r'\s*"(?P<request>[^"]*)"\s*',
         r'\s*(?P<status_code>\S+)',
         r'\s*(?P<file_size>\d+)'
     )
-    info = {
+    infoData = {
         'status_code': 0,
         'file_size': 0,
     }
-    log_fmt = '{}\\-{}{}{}{}\\s*'.format(fp[0], fp[1], fp[2], fp[3], fp[4])
+    log_fmt = '{}\\-{}{}{}{}\\s*'.format(pattern[0], pattern[1], pattern[2], pattern[3], pattern[4])
     resp_match = re.fullmatch(log_fmt, input_line)
     if resp_match is not None:
         status_code = resp_match.group('status_code')
         file_size = int(resp_match.group('file_size'))
-        info['status_code'] = status_code
-        info['file_size'] = file_size
-    return info
+        infoData['status_code'] = status_code
+        infoData['file_size'] = file_size
+    return infoData
 
 
 def print_statistics(total_file_size, status_codes_stats):
@@ -33,9 +33,9 @@ def print_statistics(total_file_size, status_codes_stats):
     '''
     print('File size: {:d}'.format(total_file_size), flush=True)
     for status_code in sorted(status_codes_stats.keys()):
-        num = status_codes_stats.get(status_code, 0)
-        if num > 0:
-            print('{:s}: {:d}'.format(status_code, num), flush=True)
+        number = status_codes_stats.get(status_code, 0)
+        if number > 0:
+            print('{:s}: {:d}'.format(status_code, number), flush=True)
 
 
 def update_metrics(line, total_file_size, status_codes_stats):
@@ -51,7 +51,7 @@ def update_metrics(line, total_file_size, status_codes_stats):
 def run():
     '''Starting the log parser.
     '''
-    line_num = 0
+    line_number = 0
     total_file_size = 0
     status_codes_stats = {
         '200': 0,
@@ -71,8 +71,8 @@ def run():
                 total_file_size,
                 status_codes_stats,
             )
-            line_num += 1
-            if line_num % 10 == 0:
+            line_number += 1
+            if line_number % 10 == 0:
                 print_statistics(total_file_size, status_codes_stats)
     except (KeyboardInterrupt, EOFError):
         print_statistics(total_file_size, status_codes_stats)
